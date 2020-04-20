0fa22a8ec9d1c6568da73c7b150b09adf73cf4f0
liguangcheng666 <1042561833@qq.com>
2020-04-20 18:48:55 +0800

    分次查询，提高效率

--- move.php
+++ move.php
@@ -2,13 +2,8 @@
 // 连接数据库、设置字符集
 include 'config.php';
 
-
 $maxID = $_GET['maxID'];
 //每次都请求新的聊天内容（不需要旧的信息）
-//本次请求的记录结果id需要大于上次“已经获得记录的最大id”
-
-//编写查询sql语句
-// $sql = "select * from message where id >'$maxID'";
 
 // 创建预处理语句
 $stmt=mysqli_stmt_init($link);
